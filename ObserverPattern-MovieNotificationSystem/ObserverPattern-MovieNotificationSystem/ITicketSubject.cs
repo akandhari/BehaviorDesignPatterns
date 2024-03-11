665b3e94aa20c90fd2ca1112dd446556a8addef6
Aakash Kandhari <aakash_kandhari@yahoo.com>
2024-03-11 18:07:09 -0500

    Delete ObserverPattern-MovieNotificationSystem/ObserverPattern-MovieNotificationSystem directory

--- ObserverPattern-MovieNotificationSystem/ObserverPattern-MovieNotificationSystem/ITicketSubject.cs
+++
@@ -1,6 +0,0 @@
-﻿public interface ITicketSubject
-{
-    void RegisterObserver(ITicketObserver observer);
-    void RemoveObserver(ITicketObserver observer);
-    void NotifyObservers(string message);
-}
